955c4add594321b20a2cc7698e7447b4c7815b77
Lucas <lxavier.dev@hotmail.com>
2025-05-14 19:21:39 -0300

    New Motorcycles added

--- src/database/motorcyles.js
+++ src/database/motorcyles.js
@@ -1,8 +1,14 @@
 import motorcycle1 from '../assets/motorcycle1.avif';
+import motorcycle2 from '../assets/motorcycle2.avif';
+import motorcycle3 from '../assets/motorcycle3.avif';
+import motorcycle4 from '../assets/motorcycle4.avif';
+import motorcycle5 from '../assets/motorcycle5.avif';
+import motorcycle6 from '../assets/motorcycle6.avif';
+import motorcycle7 from '../assets/motorcycle7.avif';
+import motorcycle8 from '../assets/motorcycle8.avif';
+import motorcycle9 from '../assets/motorcycle9.avif';
 
-export const motorcycles = 
-
-[
+export const motorcycles = [
     {
         id: 1,
         name: 'S1000RR',
@@ -16,211 +22,212 @@ export const motorcycles =
     },
     {
         id: 2,
-        name: 'S1000RR',
+        name: 'Panigale V2',
         state: 'NOVA',
-        image: motorcycle1,
+        image: motorcycle2,
         year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
+        brand: 'Ducati',
+        model: 'Panigale V2',
+        power: 155,
+        price: '94.975,00'
     },
     {
         id: 3,
-        name: 'S1000RR',
+        name: 'Ninja ZX-4RR',
         state: 'NOVA',
-        image: motorcycle1,
+        image: motorcycle3,
         year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
+        brand: 'Kawasaki',
+        model: 'ZX-4RR',
+        power: 77,
+        price: '50.000,00'
     },
     {
         id: 4,
-        name: 'S1000RR',
+        name: 'YZF-R1',
         state: 'NOVA',
-        image: motorcycle1,
+        image: motorcycle4,
         year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
+        brand: 'Yamaha',
+        model: 'YZF-R1',
+        power: 200,
+        price: '128.000,00'
     },
     {
         id: 5,
-        name: 'S1000RR',
-        state: 'NOVA',
-        image: motorcycle1,
+        name: 'Diavel V4',
+        state: 'SEMINOVA',
+        image: motorcycle5,
         year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
+        brand: 'Ducati',
+        model: 'Diavel V4',
+        power: 168,
+        price: '146.000,00'
     },
     {
         id: 6,
-        name: 'S1000RR',
+        name: 'MT-07',
         state: 'NOVA',
-        image: motorcycle1,
+        image: motorcycle6,
         year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
+        brand: 'Yamaha',
+        model: 'MT-07',
+        power: 74,
+        price: '47.000,00'
     },
     {
-        id: 7,
-        name: 'S1000RR',
-        state: 'NOVA',
-        image: motorcycle1,
-        year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
+    id: 7,
+    name: 'Z900',
+    state: 'SEMINOVA',
+    image: motorcycle7,
+    year: '2022',
+    brand: 'Kawasaki',
+    model: 'Z900',
+    power: 125,
+    price: '57.000,00'
     },
     {
         id: 8,
-        name: 'S1000RR',
-        state: 'NOVA',
-        image: motorcycle1,
-        year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
+        name: 'MT-09',
+        state: 'SEMINOVA',
+        image: motorcycle8,
+        year: '2021',
+        brand: 'Yamaha',
+        model: 'MT-09',
+        power: 115,
+        price: '52.000,00'
     },
     {
         id: 9,
-        name: 'S1000RR',
-        state: 'NOVA',
-        image: motorcycle1,
-        year: '2025',
+        name: 'R nineT',
+        state: 'SEMINOVA',
+        image: motorcycle9,
+        year: '2020',
         brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
-    },
-    {
-        id: 10,
-        name: 'S1000RR',
-        state: 'NOVA',
-        image: motorcycle1,
-        year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
-    },
-    {
-        id: 11,
-        name: 'S1000RR',
-        state: 'NOVA',
-        image: motorcycle1,
-        year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
-    },
-    {
-        id: 12,
-        name: 'S1000RR',
-        state: 'NOVA',
-        image: motorcycle1,
-        year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
-    },
-    {
-        id: 13,
-        name: 'S1000RR',
-        state: 'NOVA',
-        image: motorcycle1,
-        year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
-    },
-    {
-        id: 14,
-        name: 'S1000RR',
-        state: 'NOVA',
-        image: motorcycle1,
-        year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
-    },
-    {
-        id: 15,
-        name: 'S1000RR',
-        state: 'NOVA',
-        image: motorcycle1,
-        year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
-    },
-    {
-        id: 16,
-        name: 'S1000RR',
-        state: 'NOVA',
-        image: motorcycle1,
-        year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
-    },
-    {
-        id: 17,
-        name: 'S1000RR',
-        state: 'NOVA',
-        image: motorcycle1,
-        year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
-    },
-    {
-        id: 18,
-        name: 'S1000RR',
-        state: 'NOVA',
-        image: motorcycle1,
-        year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
-    },
-    {
-        id: 19,
-        name: 'S1000RR',
-        state: 'NOVA',
-        image: motorcycle1,
-        year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
-    },
-    {
-        id: 20,
-        name: 'S1000RR',
-        state: 'NOVA',
-        image: motorcycle1,
-        year: '2025',
-        brand: 'BMW',
-        model: 'S1000RR',
-        power: 210,
-        price: '120.000,00'
+        model: 'R nineT',
+        power: 110,
+        price: '69.000,00'
     }
-]
+    // {
+    //     id: 10,
+    //     name: 'G 310 R',
+    //     state: 'SEMINOVA',
+    //     image: motorcycle10,
+    //     year: '2022',
+    //     brand: 'BMW',
+    //     model: 'G 310 R',
+    //     power: 34,
+    //     price: '27.500,00'
+    // },
+    // {
+    //     id: 11,
+    //     name: 'Versys 1000',
+    //     state: 'SEMINOVA',
+    //     image: motorcycle11,
+    //     year: '2021',
+    //     brand: 'Kawasaki',
+    //     model: 'Versys 1000',
+    //     power: 120,
+    //     price: '59.000,00'
+    // },
+    // {
+    //     id: 12,
+    //     name: 'Ninja ZX-10R',
+    //     state: 'NOVA',
+    //     image: motorcycle12,
+    //     year: '2025',
+    //     brand: 'Kawasaki',
+    //     model: 'Ninja ZX-10R',
+    //     power: 203,
+    //     price: '116.000,00'
+    // },
+    // {
+    //     id: 13,
+    //     name: 'Scrambler Icon',
+    //     state: 'SEMINOVA',
+    //     image: motorcycle13,
+    //     year: '2022',
+    //     brand: 'Ducati',
+    //     model: 'Scrambler Icon',
+    //     power: 73,
+    //     price: '53.000,00'
+    // },
+    // {
+    //     id: 14,
+    //     name: 'Tracer 900',
+    //     state: 'SEMINOVA',
+    //     image: motorcycle14,
+    //     year: '2020',
+    //     brand: 'Yamaha',
+    //     model: 'Tracer 900',
+    //     power: 115,
+    //     price: '48.000,00'
+    // },
+    // {
+    //     id: 15,
+    //     name: 'F 900 R',
+    //     state: 'NOVA',
+    //     image: motorcycle15,
+    //     year: '2025',
+    //     brand: 'BMW',
+    //     model: 'F 900 R',
+    //     power: 105,
+    //     price: '65.000,00'
+    // },
+    // {
+    //     id: 16,
+    //     name: 'R 1250 R',
+    //     state: 'NOVA',
+    //     image: motorcycle16,
+    //     year: '2025',
+    //     brand: 'BMW',
+    //     model: 'R 1250 R',
+    //     power: 136,
+    //     price: '92.000,00'
+    // },
+    // {
+    //     id: 17,
+    //     name: 'Panigale V2',
+    //     state: 'NOVA',
+    //     image: motorcycle17,
+    //     year: '2025',
+    //     brand: 'Ducati',
+    //     model: 'Panigale V2',
+    //     power: 155,
+    //     price: '94.975,00'
+    // },
+    // {
+    //     id: 18,
+    //     name: 'Monster',
+    //     state: 'SEMINOVA',
+    //     image: motorcycle18,
+    //     year: '2021',
+    //     brand: 'Ducati',
+    //     model: 'Monster',
+    //     power: 111,
+    //     price: '66.000,00'
+    // },
+    // {
+    //     id: 19,
+    //     name: 'Z650',
+    //     state: 'SEMINOVA',
+    //     image: motorcycle19,
+    //     year: '2022',
+    //     brand: 'Kawasaki',
+    //     model: 'Z650',
+    //     power: 68,
+    //     price: '37.000,00'
+    // },
+    // {
+    //     id: 20,
+    //     name: 'XSR 900',
+    //     state: 'SEMINOVA',
+    //     image: motorcycle20,
+    //     year: '2020',
+    //     brand: 'Yamaha',
+    //     model: 'XSR 900',
+    //     power: 115,
+    //     price: '45.000,00'
+    // }
+
+];
